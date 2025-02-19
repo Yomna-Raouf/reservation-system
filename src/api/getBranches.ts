efc4654671356894736f0eb9eee28444e28e843b
Yomna-Raouf <yomnaraouf99@gmail.com>
2025-02-19 23:49:19 +0200

    chore: modify branches api page size

--- src/api/getBranches.ts
+++ src/api/getBranches.ts
@@ -1,5 +1,5 @@
 import api from './api';
 
 export const getBranchesRequest = () => {
-  return api.get('branches?include[0]=sections&include[1]=sections.tables&size=10');
+  return api.get('branches?include[0]=sections&include[1]=sections.tables&per_page=10');
 };
