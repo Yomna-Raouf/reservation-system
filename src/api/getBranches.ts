1181f4d2dc9e7c8fef9236d5875ba7dede44f03a
Yomna-Raouf <yomnaraouf99@gmail.com>
2025-02-19 23:48:20 +0200

    fix: make separate chunck for each nose module

--- src/api/getBranches.ts
+++ src/api/getBranches.ts
@@ -1,5 +1,5 @@
 import api from './api';
 
 export const getBranchesRequest = () => {
-  return api.get('branches?include[0]=sections&include[1]=sections.tables');
+  return api.get('branches?include[0]=sections&include[1]=sections.tables&size=10');
 };
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,15 @@ import vue2 from '@vitejs/plugin-vue2';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
   },
   plugins: [
     vue2(),
