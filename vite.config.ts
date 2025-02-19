7cecb6a5708bd0e70f43118622f7c82fba6659bc
Yomna-Raouf <yomnaraouf99@gmail.com>
2025-02-19 23:44:14 +0200

    chore: modefy chunck size

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue2 from '@vitejs/plugin-vue2';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 600,
+    chunkSizeWarningLimit: 1000,
   },
   plugins: [
     vue2(),
